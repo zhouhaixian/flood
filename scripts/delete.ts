b64d59cbd38a9ecccd05c10bc61a267fea1cf8cb
zhouhaixian <2223540637@qq.com>
2022-11-27 14:48:36 +0800

    chore: 完善 "delete" 脚本

--- scripts/delete.ts
+++ scripts/delete.ts
@@ -1,8 +1,13 @@
-import fs from 'fs/promises';
+import fs from 'fs';
 import path from 'path';
 
 const fileName = process.argv[2];
 if (fileName === undefined) throw Error('文件名不能为空!');
-fs.rm(path.resolve(__dirname, `../src/api/${fileName}.ts`)).catch((reason) => {
-  throw Error(reason);
-});
+const filePath = path.resolve(__dirname, `../src/api/${fileName}.ts`);
+if (fileName === undefined) throw Error('文件名不能为空!');
+try {
+  fs.accessSync(filePath), fs.constants.F_OK;
+  fs.rmSync(filePath);
+} catch {
+  throw Error(`${filePath} 不是个目录或文件`);
+}
